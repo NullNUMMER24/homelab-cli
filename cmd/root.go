6acc8fb693500c3d2d532cb4e1142ee753c3d858
NullNUMMER24 <j.rohrbach@sensemail.ch>
2025-04-13 18:32:48 +0200

    created cli structure

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var ErrorColor string = "\033[38;5;9m"
+var NoColor string = "\033[0m"
+var SuccessColor string = "\033[38;5;10m"
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "homelab-cli",
+	Short: "A simple CLI for managing your homelab",
+	Long:  `homelab-cli aims to be a simple solution to create new nixos vms on proxmox. Together with https://github.com/NullNUMMER24/nix-server-conf-manager managing a homelab with nixos should be a breeze.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.homelab-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "homelab-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
